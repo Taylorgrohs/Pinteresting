775b6d9f1dbaa0d67726488a32fd47d9283588b5
Taylor Grohs <Taylorgrohs@gmail.com>
2015-08-03 14:08:55 -0600

    Added colors and signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about' => 'pages#about'
+  get 'sign_up' => 'pages#sign_up'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
