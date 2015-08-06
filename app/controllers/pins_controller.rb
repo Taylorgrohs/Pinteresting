5952f3eae375934ac5058351b3b74203bbdf1940
Taylor Grohs <Taylorgrohs@gmail.com>
2015-08-06 15:23:29 -0600

    Changed order in which images show

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -3,7 +3,7 @@ before_action :set_pin, only: [:show, :edit, :update, :destroy]
 before_action :correct_user, only: [:edit, :update, :destroy]
 before_action :authenticate_user!, except: [:index, :show]
   def index
-    @pins = Pin.all
+    @pins = Pin.all.order("created_at DESC")
   end
 
   def show
