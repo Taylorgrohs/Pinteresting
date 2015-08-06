1e791415bae63b78bf0f66a73401de0bc493312f
Taylor Grohs <Taylorgrohs@gmail.com>
2015-08-06 14:39:11 -0600

    Resizing images

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :image, :styles => { :medium => "700x700>", :thumb => "100x100>"}	
+	has_attached_file :image, :styles => { :medium => "1100x1100>", :thumb => "300x300>"}	
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
